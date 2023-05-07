c2a174ce3399f5db0f6ddb73698d777951d44741
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-07 21:57:14 +0100

    update

--- main.js
+++ main.js
@@ -1,62 +1,60 @@
 const fs = require("fs");
-const axios = require("axios").default;
+const axios = require("axios");
 
 const baseURL = "https://api.themoviedb.org/3/";
 const apiKey = "?api_key=c4595d871c9878d6905a4403ba109c57";
 
 console.log("require OK");
 
-const randomPlatform = () => {
-  let arr = [
-    "Netflix",
-    "Amazon Prime Video",
-    "Hulu",
-    "Disney+",
-    "HBO Max",
-    "Peacock",
-    "Apple TV+",
-    "Paramount+",
-    "Crave",
-  ];
-  const str = arr[Math.floor(Math.random() * (arr.length - 1))];
-  return str;
+const page = 1;
+
+const options2 = {
+  method: "GET",
+  url: "https://api.themoviedb.org/3/genre/movie/list?language=en",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDU5NWQ4NzFjOTg3OGQ2OTA1YTQ0MDNiYTEwOWM1NyIsInN1YiI6IjY0NDdlMTYwZWY5ZDcyMDRhM2E2YmZjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uJcxkptcKhf0a0-2legOYpLrgdnn2KJ07AmVf9u21r4",
+  },
 };
 
-const data = JSON.parse(
-  fs.readFileSync("./json/tv_series_ids_04_24_2023.json", "utf-8")
-);
+const genName = async (num) => {
+  const res = await axios.request(options2);
+  const genres = res.data.genres;
+  const result = genres.filter((a) => a.id === num);
+  /*   console.log(result);
+   */ return result[0].name;
+};
 
-async function dataDump(data) {
-  await fs.appendFileSync("./tv_series.json", `{"tv_series":[\n`);
-  const index = await data.map((a) => {
-    return a.id;
+const options = {
+  method: "GET",
+  url: `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`,
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDU5NWQ4NzFjOTg3OGQ2OTA1YTQ0MDNiYTEwOWM1NyIsInN1YiI6IjY0NDdlMTYwZWY5ZDcyMDRhM2E2YmZjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uJcxkptcKhf0a0-2legOYpLrgdnn2KJ07AmVf9u21r4",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    const res = response.data.results[0];
+    console.log(response.data.results.length);
+    console.log({
+      originalId: res.id,
+      title: res.title,
+      description: res.overview,
+      type: "movie",
+      season: 1,
+      season_episodes: 1,
+      categories: "String" /* genName(res.genre_ids[0]) */,
+      platform: "String",
+      image: "https://image.tmdb.org/t/p/original/" + res.poster_path,
+    });
+  })
+  .catch(function (error) {
+    console.error(error);
   });
-  console.log(index);
-  const rng = () => {
-    return Math.floor(Math.random() * (index.length - 1));
-  };
-  console.log(rng());
 
-  for (let i = 0; i < 100; i++) {
-    const element = await axios.get(baseURL + `tv/${index[rng()]}` + apiKey);
-
-    const elementResult = {
-      title: element.data.name,
-      description: element.data.overview.slice(0, 200) + "...",
-      season: element.data.number_of_seasons,
-      season_episodes: element.data.number_of_episodes,
-      category:
-        typeof element.data.genres[0] !== "undefined"
-          ? element.data.genres[0].name
-          : "Other",
-      platform: randomPlatform(),
-      type: "show",
-    };
-    await fs.appendFileSync("./tv_series.json", JSON.stringify(elementResult));
-    await fs.appendFileSync("./tv_series.json", ", \n");
-  }
-  await fs.appendFileSync("./tv_series.json", "]}");
-  clearInterval("timer");
-}
-
-dataDump(data.tv_series);
+genName(28).then((a) => console.log(a));
