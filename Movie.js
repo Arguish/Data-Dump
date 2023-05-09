38e7dc075657392da26d7d06d032f2d08b8c72d6
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-09 16:16:43 +0100

    minor changes

--- Movie.js
+++ Movie.js
@@ -1,8 +1,4 @@
-const fs = require("fs");
 const axios = require("axios");
-
-const baseURL = "https://api.themoviedb.org/3/";
-const apiKey = "?api_key=c4595d871c9878d6905a4403ba109c57";
 
 console.log("require OK");
 
@@ -16,10 +12,10 @@ const genresConfig = {
   },
 };
 
-const showConfig = (page) => {
+const moviesConfig = (page) => {
   return {
     method: "GET",
-    url: `https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=${page}`,
+    url: `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`,
     headers: {
       accept: "application/json",
       Authorization:
@@ -38,18 +34,6 @@ const MFConfig = axios.create({
   },
 });
 
-const options = (id) => {
-  return {
-    method: "GET",
-    url: `https://api.themoviedb.org/3/tv/${id}?language=en-US`,
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDU5NWQ4NzFjOTg3OGQ2OTA1YTQ0MDNiYTEwOWM1NyIsInN1YiI6IjY0NDdlMTYwZWY5ZDcyMDRhM2E2YmZjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uJcxkptcKhf0a0-2legOYpLrgdnn2KJ07AmVf9u21r4",
-    },
-  };
-};
-
 const getPlatform = async () => {
   const { data } = await MFConfig.get("/platform");
   return data;
@@ -65,22 +49,21 @@ const genName = async (num) => {
   const genres = res.data.genres;
   const result = genres.filter((a) => a.id === num);
   /*   console.log(result);
-   */ return result[0] ? result[0].name : "Other";
+   */ return result[0].name;
 };
 
-const getMovie = async (num, showPage) => {
-  const response = await axios.request(showConfig(showPage));
+const getMovie = async (num, moviepage) => {
+  const response = await axios.request(moviesConfig(moviepage));
   const res = response.data.results[num];
-  const episodesRes = await axios.request(options(res.id));
-  //console.log(episodesRes.data);
+  console.log(response.data.results.length);
   return {
     originalId: res.id,
-    title: res.name,
+    title: res.title,
     description:
       res.overview.replace(/[^\s\d\w&,.]/gi, "").slice(0, 200) + "...",
-    type: "show",
-    season: episodesRes.data.number_of_seasons,
-    season_episodes: episodesRes.data.number_of_episodes,
+    type: "movie",
+    season: 1,
+    season_episodes: 1,
     category: res.genre_ids[0],
     platform: "String",
     image: "https://image.tmdb.org/t/p/original" + res.poster_path,
@@ -90,7 +73,6 @@ const getMovie = async (num, showPage) => {
 const THEREALDATADUMP = async (index, dumpPage) => {
   try {
     const getMovieRes = await getMovie(index, dumpPage);
-    console.log(getMovieRes);
     const genNameRes = await genName(getMovieRes.category);
     getMovieRes.category = await genNameRes;
     const getPlatformRes = await getPlatform();
@@ -107,7 +89,7 @@ const THEREALDATADUMP = async (index, dumpPage) => {
 };
 
 const loop = async (indexPage) => {
-  const response = await axios.request(showConfig(indexPage));
+  const response = await axios.request(moviesConfig(indexPage));
   const array = response.data.results;
   for (let index = 0; index < array.length; index++) {
     THEREALDATADUMP(index, indexPage);
